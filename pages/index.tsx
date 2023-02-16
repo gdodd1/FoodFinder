a0ba1afe856098015097dfb5a0abb75de3cbefe6
Alex Nguyen <hnguye48@vols.utk.edu>
2023-02-15 22:43:53 -0500

    add search bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import Link from "next/link";
 import Map from "./components/Map";
 import RestaurantCard from "./components/RestaurantCard";
-
+import SearchBar from "./components/Search";
+import React from "react";
 
 const IndexPage = () => {
   return (
@@ -10,19 +11,18 @@ const IndexPage = () => {
       <nav className="bg-white shadow-sm px-4 py-2 flex justify-end">
         <ul className="flex items-center">
           <li className="mr-4">
-          <Link href="./components/Login">
-              Login
-            </Link>
+            <Link href="./components/Login">Login</Link>
           </li>
           <li>
-            <Link href="./components/SignUp">
-              Signup
-            </Link>
+            <Link href="./components/SignUp">Signup</Link>
           </li>
         </ul>
       </nav>
 
-
+      {/* Google Map */}
+      <div className="container mx-auto px-4 mb-4">
+        <SearchBar />
+      </div>
       {/* Google Map */}
       <div className="container mx-auto px-4 mb-4">
         <Map />
