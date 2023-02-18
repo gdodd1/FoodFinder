b5c2debecca0345165fe0fb922d546ac54a7cd05
Alex Nguyen <hnguye48@vols.utk.edu>
2023-02-18 16:17:01 -0500

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Navbar from './components/navbar'
-import { Amplify } from 'aws-amplify'
-import config from '../src/aws-exports'
+import Navbar from './components/Navbar'
 
-
-Amplify.configure(config)
 
 function MyApp({ Component, pageProps }: AppProps) {
   return<>
