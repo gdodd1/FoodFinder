0deac9faf312709a38660ea8b97007ce37f1b754
Brian Nguyen <briannguyen9101@gmail.com>
2023-02-24 17:32:48 -0600

    Added comments to searchbar component

--- pages/components/searchbar.tsx
+++ pages/components/searchbar.tsx
@@ -4,10 +4,16 @@ import { MdSearch } from "react-icons/md"
 const Search = () => {
     return (
         <div className="flex gap-x-2 items-center h-[70px]">
+            {/* Category dropdown */}
             <Dropdown />
+
+            {/*Search box */}
             <input type="search" name="search" id="search" className="search rounded-lg 
                 w-[500px] h-[70px] p-3 outline-2 outline-none active:outline-orange-500 
-                duration-300"></input>
+                duration-300">
+            </input>
+
+            {/*Search button */}
             <button className="bg-orange-500 rounded-lg text-white text-lg px-5 h-[70px]">
                 <MdSearch />
             </button>
