7d46ce6c14f8d35721260b07af12f436332d3c4e
Brian Nguyen <briannguyen9101@gmail.com>
2023-02-24 17:20:51 -0600

    Added searchbar click effect

--- pages/components/searchbar.tsx
+++ pages/components/searchbar.tsx
@@ -3,10 +3,11 @@ import { MdSearch } from "react-icons/md"
 
 const Search = () => {
     return (
-        <div className="flex gap-x-1 items-center h-[70px]">
+        <div className="flex gap-x-2 items-center h-[70px]">
             <Dropdown />
             <input type="search" name="search" id="search" className="search rounded-lg 
-                w-[500px] h-[70px] p-3"></input>
+                w-[500px] h-[70px] p-3 outline-2 outline-none active:outline-orange-500 
+                duration-300"></input>
             <button className="bg-orange-500 rounded-lg text-white text-lg px-5 h-[70px]">
                 <MdSearch />
             </button>
