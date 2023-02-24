ae4efd79fb9249148f12d61e44b439a9cf492fb2
Brian Nguyen <briannguyen9101@gmail.com>
2023-02-24 14:02:17 -0600

    Created dropdown component

--- pages/components/dropdown.tsx
+++ pages/components/dropdown.tsx
@@ -3,33 +3,43 @@ import { MdArrowDropDown, MdArrowDropUp } from "react-icons/md";
 
 const dropdown = () => {
     const categories = [
+        "All",
         "Food",
         "Fast-Food",
         "Restaurants",
     ]
 
     const [open, setOpen] = useState(false)
+    const [selected, setSelected] = useState("")
+
+    const menuTrigger = (item: string) => {
+        if (item !== selected) {
+            setSelected(item)
+            setOpen(false)
+        }
+    }
 
     return (
         <div className="relative flex flex-col items-center w-[150px] h-[150px] rounded-lg">
             {/* Dropdown button */}
             <button onClick={() => setOpen(!open)} className="bg-white p-4 w-full flex 
                 items-center justify-between rounded-lg tracking-wide border-2 border-transparent
-                active:border-orange-500 duration-300 active:text-orange-500">Categories
+                active:border-orange-500 duration-300 active:text-orange-500">
+                {selected ? selected : "Categories"}
                 {!open ? <MdArrowDropDown className="h-8" /> : <MdArrowDropUp className="h-8" />}
             </button>
 
             {/* Category selector */}
-            {open && (
-                <ul className="bg-white absolute top-20 flex flex-col items-start rounded-lg 
-                    p-2 w-full">
-                    {categories.map((item) =>
-                        <li key={item} className="flex w-full justify-between hover:bg-gray-100 
-                            cursor-pointer rounded-r-lg border-l-transparent hover:border-l-orange-500 
-                            border-l-4 p-2">{item}
-                        </li>)}
-                </ul>
-            )}
+            <ul className={`bg-white absolute top-20 flex flex-col items-start rounded-lg
+                            p-2 w-full ${open ? 'max-h-32' : 'max-h-0 p-0 hidden'}`}>
+                {categories.map((item) =>
+                    <li onClick={() => menuTrigger(item)} key={item}
+                        className="flex w-full justify-between hover:bg-gray-100 cursor-pointer 
+                            rounded-r-lg border-l-transparent hover:border-l-orange-500 border-l-4 p-2
+                            active:text-orange-500 duration-150">
+                        {item}
+                    </li>)}
+            </ul>
         </div>
     )
 }
